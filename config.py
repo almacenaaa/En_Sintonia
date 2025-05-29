afd394efa7a45dc4eefdf6e055a2a6e2185dc440
almacenaaa <almacenacueducto2024@gmail.com>
2025-05-29 08:30:25 -0500

    config.py

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    MONGO_URI = os.getenv('MONGO_URI')
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    MONGO_URI = os.getenv("MONGO_URI")
