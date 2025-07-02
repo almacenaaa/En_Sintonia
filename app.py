caecbb179168fd7a952ccba27b2c0ba108f4687f
almacenaaa <almacenacueducto2024@gmail.com>
2025-07-02 14:41:18 -0500

    app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def admin_panel():
         'Críticos', 'Custodia', 'Hierro_Ductil', 'Cajas_medidores', 'Suministro_EPP', 'Dotación',
         'Abrazaderas_metálicas', 'Válvulas_mariposa', 'Tubería_ACC_PVC', 'Accesorios_PEAD',
         'Tubería_PEAD', 'Lubricantes', 'Llantería', 'Tornillería',
-        'Materiales_compactación', 'General', 'Tapabocas', 'Válvulas_laton', 'Hierro_Ductil_3', 'Hierro_Ductil_3'
+        'Materiales_compactación', 'General', 'Tapabocas', 'Válvulas_laton', 'Hierro_Ductil_2', 'Hierro_Ductil_3'
     ]
 
     if request.method == 'POST':
